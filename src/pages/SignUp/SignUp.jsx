fc36fe7de83249033e02b93dd064b5f4ab228d4e
Khairul Alam <khairul431743@gmail.com>
2023-12-26 12:57:19 +0600

    user information is addd to server

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -4,10 +4,12 @@ import { useForm } from "react-hook-form"
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
 import Swal from "sweetalert2";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
 const SignUp = () => {
+    const axiosPublic = useAxiosPublic();
     const navigate = useNavigate();
-    const {createUser, updateUsersProfile} = useContext(AuthContext);
+    const { createUser, updateUsersProfile } = useContext(AuthContext);
     const {
         register,
         handleSubmit,
@@ -16,25 +18,36 @@ const SignUp = () => {
     } = useForm()
 
     const onSubmit = (data) => {
-        console.log(data);
+
         createUser(data.email, data.password)
-        .then(result => {
-            console.log(result.user);
-            updateUsersProfile(data.name, data.photoURL)
-            .then(()=>{
-                console.log('user profile info updated');
-                reset();
-                Swal.fire({
-                    position: "top-end",
-                    icon: "success",
-                    title: "User created successfully",
-                    showConfirmButton: false,
-                    timer: 1500
-                  });
-                navigate('/');
+            .then(result => {
+                console.log(result.user);
+                updateUsersProfile(data.name, data.photoURL)
+                    .then(() => {
+                        // create user entry in the database
+                        const userInfo = {
+                            name: data.name,
+                            email: data.email,
+                        }
+                        axiosPublic.post('/users', userInfo)
+                            .then(res => {
+                                if (res.data.insertedId) {
+                                    console.log('user added to the database');
+                                    reset();
+                                    Swal.fire({
+                                        position: "top-end",
+                                        icon: "success",
+                                        title: "User created successfully",
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    });
+                                    navigate('/');
+                                }
+                            })
+
+                    })
+                    .catch(error => console.log(error))
             })
-            .catch(error => console.log(error))
-        })
     }
 
     return (
@@ -51,7 +64,7 @@ const SignUp = () => {
                     <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
                         <h1 className="text-5xl font-bold text-center py-3">Sign Up</h1>
                         <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-                            
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
@@ -70,7 +83,7 @@ const SignUp = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" {...register("email", { required: true })} placeholder="email" name="email" className="input input-bordered"  />
+                                <input type="email" {...register("email", { required: true })} placeholder="email" name="email" className="input input-bordered" />
                                 {errors.email && <span className="text-red-600">Email field is required</span>}
                             </div>
                             <div className="form-control">
@@ -78,11 +91,12 @@ const SignUp = () => {
                                     <span className="label-text">Password</span>
                                 </label>
                                 <input type="password" {...register("password", {
-                                    required:true, 
-                                    minLength: 6, 
+                                    required: true,
+                                    minLength: 6,
                                     pattern: /(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]/,
-                                    maxLength:20} )} 
-                                    name="password" placeholder="password" className="input input-bordered"  />
+                                    maxLength: 20
+                                })}
+                                    name="password" placeholder="password" className="input input-bordered" />
                                 {errors.password?.type === 'required' && (
                                     <p className="text-red-600">Password is required</p>
                                 )}
@@ -97,7 +111,7 @@ const SignUp = () => {
                                 )}
 
                             </div>
-                            
+
                             <div className="form-control mt-6">
                                 <input className="btn btn-primary" type="submit" value="Sign Up" />
                             </div>
