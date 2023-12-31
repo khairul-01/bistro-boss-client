d2f662d4d072aac2f8096f889b4b621946c22628
Khairul Alam <khairul431743@gmail.com>
2023-12-31 19:07:34 +0600

    added AddItem Page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -10,6 +10,8 @@ import PrivateRoute from "./PrivateRoute";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../pages/Dashboard/Cart/Cart";
 import AllUsers from "../pages/Dashboard/Cart/AllUsers/AllUsers";
+import AddItem from "../pages/Dashboard/AddItem/AddItem";
+import AdminRoute from "./AdminRoute";
 
 
 export const router = createBrowserRouter([
@@ -47,15 +49,20 @@ export const router = createBrowserRouter([
     path: "dashboard",
     element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
     children: [
+      // normal user routes
       {
         path: "cart",
         element: <Cart></Cart>,
       },
 
-      // admin routes
+      // admin only routes
+      {
+        path: "addItems",
+        element: <AdminRoute><AddItem></AddItem></AdminRoute>,
+      },
       {
         path: "users",
-        element: <AllUsers></AllUsers>,
+        element: <AdminRoute><AllUsers></AllUsers></AdminRoute>,
       }
     ]
    },
